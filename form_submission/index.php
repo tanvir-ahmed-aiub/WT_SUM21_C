20a698751e113265e6490b96369e3e37abe24548
taanveerahmeed <taanveerahmeed@gmail.com>
2021-06-15 16:03:50 +0600

    form validation

--- form_submission/index.php
+++ form_submission/index.php
@@ -1,20 +1,91 @@
 <?php
+	$name="";
+	$err_name="";
+	$username="";
+	$err_username="";
+	$password="";
+	$err_password="";
+	$gender="";
+	$err_gender="";
+	$profession="";
+	$err_profession="";
+	$hobbies=[];
+	$err_hobbies="";
+	$bio="";
+	$err_bio="";
+	
+	$hasError=false;
+	
+	$array= array("Teaching","Business","Service","Nothing");
+	
+	function hobbyExist($hobby){
+		global $hobbies;
+		foreach($hobbies as $h){
+			if($h == $hobby) return true;
+		}
+		return false;
+	}
+	
 	//if($_SERVER["REQUEST_METHOD"]=="POST"){
 	if(isset($_POST["submit"])){
-		echo "<h1>Form submitted</h1>";
-		echo $_POST["name"]."<br>";
-		echo $_POST["username"]."<br>";
-		echo $_POST["password"]."<br>";
-		echo $_POST["gender"]."<br>";
-		echo $_POST["profession"]."<br>";
-		echo $_POST["bio"]."<br>";
-		$arr = $_POST["hobbies"];
-
-		foreach($arr as $e){
-			echo "$e<br>";
+		if(empty($_POST["name"])){
+			$hasError = true;
+			$err_name="Name Required";
 		}
+		else if(strlen($_POST["name"]) <= 2){
+			$hasError = true;
+			$err_name="Name must contain >2 character";
+		}
+		else{
+			$name = $_POST["name"];
+		}
+		if(!isset($_POST["gender"])){
+			$hasError = true;
+			$err_gender="Gender Required";
+		}
+		else{
+			$gender = $_POST["gender"];
+		}
+		if(!isset($_POST["hobbies"])){
+			$hasError = true;
+			$err_hobbies="Hobbies Required";
+		}
+		else{
+			$hobbies = $_POST["hobbies"];
+		}
+		if (!isset($_POST["profession"])){
+			$hasError = true;
+			$err_profession="Profession Required";
+		}
+		else{
+			$profession = $_POST["profession"];
+		}
+		if(empty($_POST["bio"])){
+			$hasError = true;
+			$err_bio = "Bio Required";
+		}
+		else{
+			$bio = $_POST["bio"];
+		}
+		
+		
+		if(!$hasError){
+			echo "<h1>Form submitted</h1>";
+			echo $_POST["name"]."<br>";
+			echo $_POST["username"]."<br>";
+			echo $_POST["password"]."<br>";
+			echo $_POST["gender"]."<br>";
+			echo $_POST["profession"]."<br>";
+			echo $_POST["bio"]."<br>";
+			$arr = $_POST["hobbies"];
+
+			foreach($arr as $e){
+				echo "$e<br>";
+			}
+		}
+		
 		//we will otherwise DB CRUD or authenticate
-		header("Location: index.php");
+		///header("Location: index.php");
 	}
 	
 ?>
@@ -26,40 +97,52 @@
 			<table>
 				<tr>
 					<td>Name</td>
-					<td>: <input type="text" name="name" placeholder="Your name ...."> </td>
+					<td>: <input type="text" name="name" value="<?php echo $name; ?>" placeholder="Your name ...."> </td>
+					<td><span> <?php echo $err_name;?> </span></td>
 				</tr>
 				<tr>
 					<td>Username</td>
 					<td>: <input type="text" name="username" placeholder="Username">  </td>
+					<td><span> <?php echo $err_username;?> </span></td>
 				</tr>
 				<tr>
 					<td>Password</td>
 					<td>: <input type="password" name="password" placeholder="Password">  </td>
+					<td><span> <?php echo $err_password;?> </span></td>
 				</tr>
 				<tr>
 					<td>Gender</td>
-					<td>: <input type="radio" value="Male" name="gender"> Male <input name="gender" value="Female" type="radio"> Female </td>
+					<td>: <input type="radio" value="Male" <?php if($gender=="Male") echo "checked"; ?> name="gender"> Male <input name="gender" <?php if($gender=="Female") echo "checked"; ?> value="Female" type="radio"> Female </td>
+					<td><span> <?php echo $err_gender;?> </span></td>
 				</tr>
 				<tr>
 					<td>Profession</td>
 					<td>: <select name="profession">
-						<option>Teaching</option>
-						<option>Business</option>
-						<option>Service</option>
-						<option>Nothing</option>
+						<option disabled selected>---Select---</option>
+						<?php
+							foreach($array as $p){
+								if($p == $profession) 
+									echo "<option selected>$p</option>";
+								else
+									echo "<option>$p</option>";
+							}
+						?>
 						</select>
 					</td>
+					<td><span> <?php echo $err_profession;?> </span></td>
 				</tr>
 				<tr>
 					<td>Hobbies</td>
-					<td>: <input type="checkbox" name="hobbies[]" value="Movies"> Movies 
-					<input type="checkbox" name="hobbies[]" value="Music"> Music
-					<input type="checkbox" name="hobbies[]" value="Sports"> Sports
+					<td>: <input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Movies")) echo "checked";?> value="Movies"> Movies 
+					<input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Music")) echo "checked";?> value="Music"> Music<br>
+					<input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Sports")) echo "checked";?> value="Sports"> Sports
 					</td>
+					<td><span> <?php echo $err_hobbies;?> </span></td>
 				</tr>
 				<tr>
 					<td>Bio</td>
-					<td>: <textarea name="bio"></textarea>
+					<td>: <textarea name="bio" ><?php echo $bio; ?></textarea>
+					<td><span> <?php echo $err_bio;?> </span></td>
 					</td>
 				</tr>
 				<tr>
